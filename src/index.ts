6598681d0e2cb371cdbb1a235e0b14d2c803e3f5
Tronikel <contactdonatas@gmail.com>
2024-05-31 20:14:16 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+type Task<T> = {
+    fn: () => Promise<T>;
+    success: (arg: T) => void;
+    failure: (reason: unknown) => void;
+};
+
+export default class AsyncQueue {
+    private concurrency = 5;
+    private running: Promise<unknown>[] = [];
+    private waitlist: Promise<void>[] = [];
+    private stack = 0;
+
+    constructor(concurrency?: number) {
+        if (concurrency !== undefined) {
+            this.concurrency = concurrency;
+        }
+    }
+
+    run<T>(fn: () => Promise<T>): Promise<T> {
+        return new Promise((success, failure) => {
+            void this.processTask({
+                fn,
+                success,
+                failure,
+            });
+        });
+    }
+
+    private async processTask<T>(task: Task<T>) {
+        this.stack++;
+
+        let index = this.running.length;
+
+        if (this.running.length >= this.concurrency) {
+            const last = this.waitlist.at(-1);
+            let done = () => {};
+
+            this.waitlist.push(
+                new Promise(res => {
+                    done = res;
+                })
+            );
+
+            if (last) {
+                await last;
+                void this.waitlist.pop();
+            }
+
+            index = await Promise.race(
+                this.running.map(async (promise, index) => {
+                    try {
+                        await promise;
+                    } catch {}
+                    return index;
+                })
+            );
+
+            done();
+        }
+
+        try {
+            const promise = task.fn();
+            this.running[index] = promise;
+            task.success(await promise);
+        } catch (err) {
+            task.failure(err);
+        }
+
+        this.stack--;
+
+        if (this.stack === 0) {
+            this.waitlist = [];
+            this.running = [];
+        }
+    }
+}
